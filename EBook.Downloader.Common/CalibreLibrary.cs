49f4a824d7c32b9403735174535e5098e8d8f87f
Ross King <rsking@gmail.com>
2018-05-14 13:40:47 +1000

    Updated to use calibredb when possible

--- EBook.Downloader.Common/CalibreLibrary.cs
+++ EBook.Downloader.Common/CalibreLibrary.cs
@@ -18,13 +18,19 @@ namespace EBook.Downloader.Common
     /// </summary>
     public class CalibreLibrary : IDisposable
     {
+        private const string DefaultCalibrePath = "C:\\Program Files\\Calibre2";
+
         private const string TriggerName = "books_update_trg";
 
         private readonly ILogger logger;
 
+        private readonly string calibreDbPath;
+
         private Microsoft.Data.Sqlite.SqliteConnection connection;
 
-        private Microsoft.Data.Sqlite.SqliteCommand selectCommand;
+        private Microsoft.Data.Sqlite.SqliteCommand selectBookWithFormatCommand;
+
+        private Microsoft.Data.Sqlite.SqliteCommand selectBookWithoutFormatCommand;
 
         private Microsoft.Data.Sqlite.SqliteCommand updateCommand;
 
@@ -38,29 +44,39 @@ namespace EBook.Downloader.Common
         /// Initializes a new instance of the <see cref="CalibreLibrary" /> class.
         /// </summary>
         /// <param name="path">The path.</param>
-        /// <param name="logger">The logger.static</param>
-        public CalibreLibrary(string path, ILogger logger)
+        /// <param name="logger">The logger.</param>
+        /// <param name="calibrePath">The path to the calibre binaries.</param>
+        public CalibreLibrary(string path, ILogger logger, string calibrePath = DefaultCalibrePath)
         {
             this.Path = path;
             this.logger = logger;
+            this.calibreDbPath = System.IO.Path.Combine(calibrePath, "calibredb.exe");
 
-            var connectionStringBuilder = new Microsoft.Data.Sqlite.SqliteConnectionStringBuilder { DataSource = System.IO.Path.Combine(path, "metadata.db") };
+            var connectionStringBuilder = new Microsoft.Data.Sqlite.SqliteConnectionStringBuilder { DataSource = System.IO.Path.Combine(path, "metadata.db"), Mode = Microsoft.Data.Sqlite.SqliteOpenMode.ReadWrite };
             this.connection = new Microsoft.Data.Sqlite.SqliteConnection(connectionStringBuilder.ConnectionString);
             this.connection.Open();
 
-            this.selectCommand = this.connection.CreateCommand();
-            this.selectCommand.CommandText = "SELECT b.id, b.path, d.name, b.last_modified FROM books b INNER JOIN data d ON b.id = d.book INNER JOIN books_publishers_link bpl ON b.id = bpl.book INNER JOIN publishers p ON bpl.publisher = p.id INNER JOIN books_authors_link bal ON b.id = bal.book INNER JOIN authors a ON bal.author = a.id WHERE d.format = :extension AND a.name = :author AND b.title = :title AND p.name = :publisher";
-            this.selectCommand.Parameters.Add(":extension", Microsoft.Data.Sqlite.SqliteType.Text);
-            this.selectCommand.Parameters.Add(":author", Microsoft.Data.Sqlite.SqliteType.Text);
-            this.selectCommand.Parameters.Add(":title", Microsoft.Data.Sqlite.SqliteType.Text);
-            this.selectCommand.Parameters.Add(":publisher", Microsoft.Data.Sqlite.SqliteType.Text);
-            this.selectCommand.Prepare();
+            this.selectBookWithoutFormatCommand = this.connection.CreateCommand();
+            this.selectBookWithoutFormatCommand.CommandText = "SELECT b.id, b.path, d.name, b.last_modified FROM books b INNER JOIN data d ON b.id = d.book INNER JOIN books_publishers_link bpl ON b.id = bpl.book INNER JOIN publishers p ON bpl.publisher = p.id INNER JOIN books_authors_link bal ON b.id = bal.book INNER JOIN authors a ON bal.author = a.id WHERE a.name = :author AND b.title = :title AND p.name = :publisher";
+            this.selectBookWithoutFormatCommand.Parameters.Add(":author", Microsoft.Data.Sqlite.SqliteType.Text);
+            this.selectBookWithoutFormatCommand.Parameters.Add(":title", Microsoft.Data.Sqlite.SqliteType.Text);
+            this.selectBookWithoutFormatCommand.Parameters.Add(":publisher", Microsoft.Data.Sqlite.SqliteType.Text);
+            this.selectBookWithoutFormatCommand.Prepare();
+
+            this.selectBookWithFormatCommand = this.connection.CreateCommand();
+            this.selectBookWithFormatCommand.CommandText = this.selectBookWithoutFormatCommand.CommandText + " AND d.format = :extension";
+            foreach (Microsoft.Data.Sqlite.SqliteParameter parameter in this.selectBookWithoutFormatCommand.Parameters)
+            {
+                this.selectBookWithFormatCommand.Parameters.Add(parameter.ParameterName, parameter.SqliteType);
+            }
+
+            this.selectBookWithFormatCommand.Parameters.Add(":extension", Microsoft.Data.Sqlite.SqliteType.Text);
+            this.selectBookWithFormatCommand.Prepare();
 
             this.updateCommand = this.connection.CreateCommand();
             this.updateCommand.CommandText = "UPDATE books SET last_modified = :lastModified WHERE id = :id";
             this.updateCommand.Parameters.Add(":lastModified", Microsoft.Data.Sqlite.SqliteType.Text);
             this.updateCommand.Parameters.AddWithValue(":id", Microsoft.Data.Sqlite.SqliteType.Integer);
-
 
             string createTriggerCommandText = default;
             using (var command = this.connection.CreateCommand())
@@ -94,12 +110,13 @@ namespace EBook.Downloader.Common
             string path = default;
             string name = default;
             string lastModified = default;
-            this.selectCommand.Parameters[":extension"].Value = info.Extension.ToUpperInvariant();
-            this.selectCommand.Parameters[":author"].Value = info.Authors.First().Replace(',', '|');
-            this.selectCommand.Parameters[":title"].Value = info.Title;
-            this.selectCommand.Parameters[":publisher"].Value = info.Publisher;
+            
+            this.selectBookWithFormatCommand.Parameters[":author"].Value = info.Authors.First().Replace(',', '|');
+            this.selectBookWithFormatCommand.Parameters[":title"].Value = info.Title;
+            this.selectBookWithFormatCommand.Parameters[":publisher"].Value = info.Publisher;
+            this.selectBookWithFormatCommand.Parameters[":extension"].Value = info.Extension.ToUpperInvariant();
 
-            using (var reader = await this.selectCommand.ExecuteReaderAsync())
+            using (var reader = await this.selectBookWithFormatCommand.ExecuteReaderAsync())
             {
                 if (await reader.ReadAsync())
                 {
@@ -112,51 +129,69 @@ namespace EBook.Downloader.Common
 
             if (id == 0 || path == null || name == null)
             {
-                return false;
-            }
+                // see if we need to add the book or add the format
+                this.selectBookWithoutFormatCommand.Parameters[":author"].Value = info.Authors.First().Replace(',', '|');
+                this.selectBookWithoutFormatCommand.Parameters[":title"].Value = info.Title;
+                this.selectBookWithoutFormatCommand.Parameters[":publisher"].Value = info.Publisher;
 
-            var fullPath = System.IO.Path.Combine(this.Path, path, string.Format("{0}{1}", name, System.IO.Path.GetExtension(info.Path)));
-
-            if (System.IO.File.Exists(fullPath))
-            {
-                // see if this has changed at all
-                if (!CheckFiles(info.Path, fullPath, this.logger))
+                using (var reader = await this.selectBookWithoutFormatCommand.ExecuteReaderAsync())
                 {
-                    // files are not the same. Copy in the new file
-                    this.logger.LogInformation("\tReplacing {0} as files do not match", name);
-                    System.IO.File.Copy(info.Path, fullPath, true);
-                }
-
-                // see if we need to update the last modified time
-                var sourceFileInfo = new System.IO.FileInfo(info.Path);
-                var sourceLastWriteTime = sourceFileInfo.LastWriteTimeUtc.ToString("yyyy-MM-dd HH:mm:ss.ffffffzzz");
-                if (sourceLastWriteTime != lastModified)
-                {
-                    // check this as date time, to be within the same minute, and is the latest date/time
-                    var lastModifiedDateTime = DateTime.Parse(lastModified);
-                    var difference = sourceFileInfo.LastWriteTime - lastModifiedDateTime;
-                    if (Math.Abs(difference.TotalMinutes) > 1D || difference.TotalMinutes > 0)
+                    if (await reader.ReadAsync())
                     {
-                        // write this to the database
-                        this.logger.LogInformation("\tUpdating last modified time for {0} in the database to {1}", name, sourceLastWriteTime);
-                        this.updateCommand.Parameters[":lastModified"].Value = sourceLastWriteTime;
-                        this.updateCommand.Parameters[":id"].Value = id;
-
-                        if (this.dropTriggerCommand != null)
-                        {
-                            await this.dropTriggerCommand.ExecuteNonQueryAsync();
-                        }
-
-                        await this.updateCommand.ExecuteNonQueryAsync();
-
-                        if (this.createTriggerCommand != null)
-                        {
-                            await this.createTriggerCommand.ExecuteNonQueryAsync();
-                        }
+                        id = reader.GetInt32(0);
+                        path = reader.GetString(1);
+                        name = reader.GetString(2);
+                        lastModified = reader.GetString(3);
                     }
                 }
 
+                if (id == 0)
+                {
+                    // we need to add this
+                    this.ExecuteCalibreDb("add", "--duplicates --languages eng \"" + info.Path + "\"");
+                    using (var reader = await this.selectBookWithoutFormatCommand.ExecuteReaderAsync())
+                    {
+                        if (await reader.ReadAsync())
+                        {
+                            id = reader.GetInt32(0);
+                            path = reader.GetString(1);
+                            name = reader.GetString(2);
+                            lastModified = reader.GetString(3);
+                        }
+                    }
+
+                    this.UpdateLastWriteTime(path, name, info);
+                    await this.UpdateLastModifiedAsync(id, name, info.Path, lastModified);
+                }
+                else
+                {
+                    // add this format
+                    this.ExecuteCalibreDb("add_format", "--dont-replace " + id + " \"" + info.Path + "\"");
+                    this.UpdateLastWriteTime(path, name, info);
+                    await this.UpdateLastModifiedAsync(id, name, info.Path, lastModified);
+                }
+
                 return true;
+            }
+            else
+            {
+                var fullPath = System.IO.Path.Combine(this.Path, path, string.Format("{0}{1}", name, System.IO.Path.GetExtension(info.Path)));
+
+                if (System.IO.File.Exists(fullPath))
+                {
+                    // see if this has changed at all
+                    if (!CheckFiles(info.Path, fullPath, this.logger))
+                    {
+                        // files are not the same. Copy in the new file
+                        this.logger.LogInformation("\tReplacing {0} as files do not match", name);
+                        System.IO.File.Copy(info.Path, fullPath, true);
+                    }
+
+                    // see if we need to update the last modified time
+                    await this.UpdateLastModifiedAsync(id, name, info.Path, lastModified);
+
+                    return true;
+                }
             }
 
             return false;
@@ -266,6 +301,91 @@ namespace EBook.Downloader.Common
                 }
 
                 return (int)value;
+            }
+        }
+
+        private async Task UpdateLastModifiedAsync(int id, string name, string path, string lastModified)
+        {
+            var sourceFileInfo = new System.IO.FileInfo(path);
+            var sourceLastWriteTime = sourceFileInfo.LastWriteTimeUtc.ToString("yyyy-MM-dd HH:mm:ss.ffffffzzz");
+            if (sourceLastWriteTime != lastModified)
+            {
+                // check this as date time, to be within the same minute, and is the latest date/time
+                var lastModifiedDateTime = DateTime.Parse(lastModified);
+                var difference = sourceFileInfo.LastWriteTime - lastModifiedDateTime;
+                if (Math.Abs(difference.TotalMinutes) > 1D || difference.TotalMinutes > 0)
+                {
+                    // write this to the database
+                    this.logger.LogInformation("\tUpdating last modified time for {0} in the database to {1}", name, sourceLastWriteTime);
+                    this.updateCommand.Parameters[":lastModified"].Value = sourceLastWriteTime;
+                    this.updateCommand.Parameters[":id"].Value = id;
+
+                    if (this.dropTriggerCommand != null)
+                    {
+                        await this.dropTriggerCommand.ExecuteNonQueryAsync();
+                    }
+
+                    await this.updateCommand.ExecuteNonQueryAsync();
+
+                    if (this.createTriggerCommand != null)
+                    {
+                        await this.createTriggerCommand.ExecuteNonQueryAsync();
+                    }
+                }
+            }
+        }
+
+        private void UpdateLastWriteTime(string path, string name, EpubInfo info)
+        {
+            var fullPath = System.IO.Path.Combine(this.Path, path, string.Format("{0}{1}", name, System.IO.Path.GetExtension(info.Path)));
+            if (System.IO.File.Exists(fullPath))
+            {
+                var fileSystemInfo = new System.IO.FileInfo(info.Path);
+                var dateTime = fileSystemInfo.LastWriteTime;
+
+                fileSystemInfo = new System.IO.FileInfo(fullPath);
+                fileSystemInfo.LastWriteTime = dateTime;
+            }
+        }
+
+        private void ExecuteCalibreDb(string command, string arguments = "")
+        {
+            try
+            {
+                var fullArguments = command + " --library-path \"" + this.Path + "\" " + arguments;
+
+                var processStartInfo = new System.Diagnostics.ProcessStartInfo(this.calibreDbPath, fullArguments);
+                processStartInfo.UseShellExecute = false;
+                processStartInfo.RedirectStandardOutput = true;
+                processStartInfo.RedirectStandardError = true;
+
+                var process = new System.Diagnostics.Process();
+                process.StartInfo = processStartInfo;
+                process.OutputDataReceived += (sender, args) =>
+                {
+                    if (args?.Data != null)
+                    {
+                        this.logger.LogInformation(0, args.Data);
+                    }
+                };
+
+                process.ErrorDataReceived += (sender, args) =>
+                {
+                    if (args?.Data != null)
+                    {
+                        this.logger.LogError(0, args.Data);
+                    }
+                };
+
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+
+                process.WaitForExit();
+            }
+            catch (System.Exception ex)
+            {
+                this.logger.LogError(0, ex, "Failed to run calibredb.exe");
             }
         }
     }
