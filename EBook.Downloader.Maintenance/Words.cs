51a1ecf1a49d192a16136354b365cde8187c4858
Ross King <rsking@gmail.com>
2021-03-10 22:34:33 +1100

    Cleaned up refactored code

--- EBook.Downloader.Maintenance/Words.cs
+++ EBook.Downloader.Maintenance/Words.cs
@@ -1,29 +1,37 @@
-﻿namespace EBook.Downloader.Maintenance
-{
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+﻿// -----------------------------------------------------------------------
+// <copyright file="Words.cs" company="RossKing">
+// Copyright (c) RossKing. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
 
+namespace EBook.Downloader.Maintenance
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Class for words.
+    /// </summary>
     internal static class Words
     {
+        /// <summary>
+        /// Words that should be lower case.
+        /// </summary>
         public static readonly IEnumerable<string> LowerCase = new[]
         {
             "a",
-             "for",
-             "of",
-             "on",
-             "and",
-             "in",
-             "the",
-             "it",
-             "it",
-             "it's",
-             "as",
-             "to",
-             "ca.",
-             "into",
+            "for",
+            "of",
+            "on",
+            "and",
+            "in",
+            "the",
+            "it",
+            "it",
+            "it's",
+            "as",
+            "to",
+            "ca.",
+            "into",
         };
     }
 }
