a94f942c226ce7fc09367b4c82395232b5b1e496
Ross King <rsking@gmail.com>
2022-01-06 10:15:37 +1100

    fix: issue with double dash being sanitised
    Lucene sanitises this to '-', whereas calibre sanitises it to '--'

--- EBook.Downloader.Common/CalibreBook.cs
+++ EBook.Downloader.Common/CalibreBook.cs
@@ -98,22 +98,28 @@ public record class CalibreBook
 
     private static char[] GetInvalidFileNameChars() => new char[]
     {
-            '\"', '<', '>', '|', '\0',
-            (char)1, (char)2, (char)3, (char)4, (char)5, (char)6, (char)7, (char)8, (char)9, (char)10,
-            (char)11, (char)12, (char)13, (char)14, (char)15, (char)16, (char)17, (char)18, (char)19, (char)20,
-            (char)21, (char)22, (char)23, (char)24, (char)25, (char)26, (char)27, (char)28, (char)29, (char)30,
-            (char)31, ':', '*', '?', '\\', '/',
+        '\"', '<', '>', '|', '\0',
+        (char)1, (char)2, (char)3, (char)4, (char)5, (char)6, (char)7, (char)8, (char)9, (char)10,
+        (char)11, (char)12, (char)13, (char)14, (char)15, (char)16, (char)17, (char)18, (char)19, (char)20,
+        (char)21, (char)22, (char)23, (char)24, (char)25, (char)26, (char)27, (char)28, (char)29, (char)30,
+        (char)31, ':', '*', '?', '\\', '/',
     };
 
     private static string Sanitise(string input)
     {
+        input = Presanitize(input);
         var length = input.Length;
-        var inputChars = input.ToCharArray();
         var outputChars = new char[4 * length];
 
-        var characters = Lucene.Net.Analysis.Miscellaneous.ASCIIFoldingFilter.FoldToASCII(inputChars, 0, outputChars, 0, length);
+        var characters = Lucene.Net.Analysis.Miscellaneous.ASCIIFoldingFilter.FoldToASCII(input.ToCharArray(), 0, outputChars, 0, length);
 
         return RemoveOthers(outputChars, characters);
+
+        static string Presanitize(string input)
+        {
+            // any special cases not convered by Lucene
+            return input.Replace("—", "--");
+        }
 
         static string RemoveOthers(char[] normalizedString, int length)
         {
