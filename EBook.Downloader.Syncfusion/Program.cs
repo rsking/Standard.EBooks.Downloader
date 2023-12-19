d3ba7c5ee8e9ec49675705b88990cf547c1f6110
Ross King <rsking@gmail.com>
2023-12-20 10:45:59 +1100

    :sparkles: sort by series index

--- EBook.Downloader.Syncfusion/Program.cs
+++ EBook.Downloader.Syncfusion/Program.cs
@@ -51,7 +51,12 @@ static async Task Process(
 {
     var logger = host.Services.GetRequiredService<ILogger<CalibreDb>>();
     var calibreDb = new CalibreDb(calibreLibraryPath.FullName, useContentServer, logger);
-    var list = await calibreDb.ListAsync(fields: new[] { "id", "title", "identifiers", "comments" }, searchPattern: "series:\"=Succinctly\"", cancellationToken: cancellationToken).ConfigureAwait(false);
+    var list = await calibreDb.ListAsync(
+        fields: new[] { "id", "title", "identifiers", "comments" },
+        sortBy: "series_index",
+        ascending: true,
+        searchPattern: "series:\"=Succinctly\"",
+        cancellationToken: cancellationToken).ConfigureAwait(false);
 
     if (list is null)
     {
